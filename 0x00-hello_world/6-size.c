68358dddb3c98a6e32511b67011cba18a0feb712
Nosipho2021 <lorrainemdk@gmail.com>
2023-02-18 04:54:13 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - A program that prints the size of various computer types
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
