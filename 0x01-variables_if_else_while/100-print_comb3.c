82cb399f4bee061034952a81546fdcfd31d4588f
Nosipho2021 <lorrainemdk@gmail.com>
2023-02-20 10:59:12 -0800

    100 print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,24 @@
  * Return: always 0
  */
 
-int msin(void)
-}
+int main(void)
+{
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
+		for (j = i ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{	
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-	putchar('\n')
+	putchar(i'\n')
 	return (0);
 }
-
