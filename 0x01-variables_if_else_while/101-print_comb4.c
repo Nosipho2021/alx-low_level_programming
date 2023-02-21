c663dfcc39e9425f1d3eb5ecdfa395eebe38bba7
Nosipho2021 <lorrainemdk@gmail.com>
2023-02-20 16:45:12 -0800

    successful combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,5 +10,27 @@ int main(void)
 	int a;
 	int b;
 	int c;
-	int d;
 
+	for (a = 0; a < 10 ; a++)
+	{
+		for (b = 1 ; b < 10; b++)
+		{
+			for (c = 2 ; c < 10 ; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
