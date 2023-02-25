38df0722d12b1938a39c4cb40f4fa8ebd598586c
Nosipho2021 <lorrainemdk@gmail.com>
2023-02-24 16:09:56 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
-void print_most_numbers(void) {
-    int i;
-    for (i = 0; i <= 9; i++) {
-        if (i != 2 && i != 4) {
-            _putchar(i + '0');
-        }
-    }
-    _putchar('\n');
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
