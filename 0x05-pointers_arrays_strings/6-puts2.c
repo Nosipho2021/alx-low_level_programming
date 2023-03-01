6348d5cb005eb3943248e2f566fc218e62da979b
Nosipho2021 <lorrainemdk@gmail.com>
2023-02-28 18:50:33 -0800

    half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * puts2 - prints one character out of two
+ * puts2 - function should print only one character out of two
+ * starting with the first one
  * @str: input
  * Return: print
  */
-void puts2(char *s)
+void puts2(char *str)
 {
-	int i = 0;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	for (; str[i] != '\0'; i++)
+	while (*y != '\0')
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue;
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
+}
