94c8b3a9d43713cc6ccfc7db681d50d353adae57
Nosipho2021 <lorrainemdk@gmail.com>
2023-03-06 17:15:18 -0800

    Double pointer, double fun

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,27 +3,25 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
-/**
- * main - main function
- *
- * Return: always 0
- */
 int main(void)
 {
 	int n;
-	int m;
+	int
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	m = n % 10;|
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, m);|
-	else if (m == 0)
-		printf("Last digit of %d and is 0", n, m);|
-	else if (m < 6 && m |= 0)
-		printf("Last digit of %d is %d and is less than 6and not 0", n, m);|
-	printf("\n")
-
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n);
+	}
 	return (0);
 }
