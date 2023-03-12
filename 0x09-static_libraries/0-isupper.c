8dc274f44ebe09f3a808ece21049bd7fca72927f
Nosipho2021 <lorrainemdk@gmail.com>
2023-03-12 08:42:31 -0700

    isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * isupper - uppercase letters
+ * _isupper - checks for uppercase letters
  * @c: char to check
  *
  * Return: 0 or 1
  */
-int_isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
