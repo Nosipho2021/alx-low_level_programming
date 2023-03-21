d09b2dff1fc773b2e6b41805cc6fc0a3aa99fd01
Nosipho2021 <lorrainemdk@gmail.com>
2023-03-21 04:36:58 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,10 @@
 	 * @age: Second member
 	 * @owner: Third member
 	 *
-	 * Description: Longer description
+	 * Description: This structure represents a dog and contains basic information
+ * such as its name, age, and owner. This structure is used throughout the
+ * program to store information about individual dogs and pass that information
+ * between functions.
 	 */
 	struct dog
 	{
