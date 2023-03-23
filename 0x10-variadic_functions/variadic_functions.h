7367002c8602fc001127bb2c5bca8a927125ae11
Nosipho2021 <lorrainemdk@gmail.com>
2023-03-23 10:43:40 -0700

    headerfile

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,22 @@
 
 #include <stdarg.h>
 
+/**
+ * struct print_format - Struct for print formats
+ * @type: The format type
+ * @func: The function pointer to the print function
+ */
+typedef struct print_format
+{
+	char *type;
+	void (*func)(va_list);
+}	print_format_t;
+
 	int _putchar(char c);
 	int sum_them_all(const unsigned int n, ...);
 	void print_numbers(const char *separator, const unsigned int n, ...);
 	void print_strings(const char *separator, const unsigned int n, ...);
 	void print_all(const char * const format, ...);
+
 	#endif
 
