ce1d922b1b247e9d1f770b87c8910a78fc876564
Nosipho2021 <lorrainemdk@gmail.com>
2023-04-09 02:14:05 -0700

    3.11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * set_bit: sets the value of a bit to 1 at a given index
+ * set_bit - sets the value of a bit to 1 at a given index
  * @index: starting from 0 of the bit you want to set
  * @n: pointer to the number to change
  * Return: 1 if successful, -1 if fails
