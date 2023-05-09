e18a6cbe81455999f005f5aa89b6ea6b218787b9
Nosipho2021 <lorrainemdk@gmail.com>
2023-05-09 07:38:09 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,50 +12,19 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
-	return (0);
-	}
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	FILE *file = fopen(filename, "r");
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	if (file == NULL)
-	{
-	return (0);
-	}
-	char *buffer = (char *) malloc(letters + 1);
-
-	if (buffer == NULL)
-	{
-	fclose(file);
-
-	return (0);
-	}
-
-	ssize_t read_count = fread(buffer, sizeof(char), letters, file);
-
-	if (ferror(file))
-	{
-	free(buffer);
-	fclose(file);
-
-	return (0);
-	}
-
-	buffer[read_count] = '\0';
-
-	ssize_t write_count = fwrite(buffer, sizeof(char), read_count, stdout);
-
-	if (write_count != read_count)
-	{
-	free(buffer);
-	fclose(file);
-	return (0);
-	}
-
-	free(buffer);
-	fclose(file);
-
-	return (read_count);
+	free(buf);
+	close(fd);
+	return (w);
 }
-
