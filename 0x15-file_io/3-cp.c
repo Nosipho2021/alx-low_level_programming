ca39eb8e486cec75698cd0226f0cd994516b3792
Nosipho2021 <lorrainemdk@gmail.com>
2023-05-10 12:13:53 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -55,8 +55,6 @@ void close_file(int fd)
  * @argv: An array of pointer to argument
  * @argc: The number of arguments supplied to the program
  */
-
-
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
@@ -64,44 +62,44 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-	if (from == -1 || r == -1)
-	{
-	dprintf(STDERR_FILENO,
-	"Error: Can't read from file %s\n", argv[1]);
-	free(buffer);
-	close_file(to);
-	exit(98);
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
 
-	}
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 
-	w = write(to, buffer, r);
-	if (to == -1 || w == -1)
-	{
-	dprintf(STDERR_FILENO,
-		"Error: Can't write to %s\n", argv[2]);
-	free(buffer);
-	close_file(to);
-	exit(99);
-	}
-
-	r = read(from, buffer, 1024);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
 	close_file(from);
 	close_file(to);
+
 	return (0);
 }
+
 
 
